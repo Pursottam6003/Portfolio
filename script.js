b541cf110fa400aef08a31e16d0af4ab310eb885
Pursottam Sah <rahulsah6003@gmail.com>
2022-02-14 19:37:40 +0530

    resigning the image

--- script.js
+++ script.js
@@ -1,64 +1,62 @@
-let mypara= document.getElementById('p1');
-let myframe= document.getElementById('frame');
-let mybody=document.getElementById('mybody');
-let myvar=0;
- 
-function loading()
-{   
+let mypara = document.getElementById('p1');
+let myframe = document.getElementById('frame');
+let mybody = document.getElementById('mybody');
+let myvar = 0;
+
+function loading() {
     console.log('inside the load');
-    myframe.style.display='block';
-    mybody.style.display='block';
+    myframe.style.display = 'block';
+    mybody.style.display = 'block';
     window.onscroll = function () { window.scrollTo(0, 0); };
-    setTimeout(function(){
-       
-        myframe.style.display='none';
-        window.onscroll='enabled';
-        
-    },5000);
+    setTimeout(function () {
+
+        myframe.style.display = 'none';
+        window.onscroll = 'enabled';
+
+    }, 2100);
 }
 
-mypara.innerHTML="Software engineer";
-function intro()
-{
+mypara.innerHTML = "Software engineer";
+function intro() {
     console.log('inside');
-    
+
     // let delaytime=2000
 
-    setInterval(function(){
-        mypara.style.transition="0.3s";
-        mypara.style.color="blue";
-        mypara.innerHTML="Web Developer";
-    },2000)
-    setInterval(function(){
-            
-        mypara.style.color="darkmagenta";
-        mypara.innerHTML="Tech Geeek";
-        mypara.style.transition="0.3s";
-        
-    },4000);
+    setInterval(function () {
+        mypara.style.transition = "0.3s";
+        mypara.style.color = "blue";
+        mypara.innerHTML = "Web Developer";
+    }, 2000)
+    setInterval(function () {
+
+        mypara.style.color = "darkmagenta";
+        mypara.innerHTML = "Tech Geeek";
+        mypara.style.transition = "0.3s";
+
+    }, 4000);
     console.log('inside the if condition');
 
-    setInterval(function(){
-        mypara.innerHTML="Software Developer";
-        mypara.style.color="darkslateblue";
-        mypara.style.transition="0.3s";
-    },5000);
+    setInterval(function () {
+        mypara.innerHTML = "Software Developer";
+        mypara.style.color = "darkslateblue";
+        mypara.style.transition = "0.3s";
+    }, 5000);
 
     console.log('inside another condtion');
 
-    setInterval(function(){
-    
-    mypara.innerHTML="Competitive Programer";
-    mypara.style.color="black";
-    mypara.style.transition="0.3s";
+    setInterval(function () {
 
-    },7000);
-    
-    setInterval(function(){
-        mypara.innerHTML="Music lover";
-        mypara.style.color="darkred";
-        mypara.style.transition="0.3s";
+        mypara.innerHTML = "Competitive Programer";
+        mypara.style.color = "black";
+        mypara.style.transition = "0.3s";
 
-    },9000);
+    }, 7000);
+
+    setInterval(function () {
+        mypara.innerHTML = "Music lover";
+        mypara.style.color = "darkred";
+        mypara.style.transition = "0.3s";
+
+    }, 9000);
 }
 
