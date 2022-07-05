03511d9d8ca02c18fab9ef892dd1e3a71e3f8817
Pursottam6003 <rahulsah6003@gmail.com>
2022-07-06 00:07:59 +0530

    updated the animation part

--- script.js
+++ script.js
@@ -60,3 +60,28 @@ function intro() {
     }, 9000);
 }
 
+
+// animation property
+function reveal(){
+    var reveals=document.querySelectorAll(".reveal");
+
+    // it selects all the elements with .reveal class and simply togles the opticity
+    for(var i=0;i<reveals.length;i++)
+    {   // to get height
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible=150;
+        if(elementTop < windowHeight - elementVisible){
+            // toggling the css property by changing the class
+            reveals[i].classList.add("active");   
+        }
+
+        else {
+            reveals[i].classList.remove("active"); 
+        }
+    }
+}
+
+window.addEventListener("scroll",reveal);
+
+ 
